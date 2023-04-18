44dff05e52763e2b0b959fd713b6e2bfa8fe2128
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-04-18 02:37:55 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tim Brady
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate() {
+  // input
+  const base = parseInt(document.getElementById("base-of-parallelogram").value)
+  const height = parseInt(document.getElementById("height-of-parallelogram").value)
+
+  // process
+  const area = (base * height)
+
+  // output
+  document.getElementById("area").innerHTML = "Area of parallelogram is: " + area + " cm²"
+}
